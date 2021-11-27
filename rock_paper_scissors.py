d44dcf77a2280c756f1bfdc632c0b9383b5b0277
Prince choudhary <choudharyprince890@gmail.com>
2021-11-27 14:52:38 +0530

    First Commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,23 @@
+import random as a 
+number=a.randint(1,3)
+
+if number==1:
+    computer='rock'
+if number==2:
+    computer='paper'    
+if number==3:
+    computer='scissor'
+
+def z():
+    print('match begin the game in 3 sec')
+    user=input('choose between (rock), (paper) & (scissor)....... ' ).lower()
+    
+    if user==computer:
+            print('match is tie')
+    elif computer=='scissor' and user=='paper' or computer=='paper' and user=='scissor' or computer=='scissor' and user=='rock':
+            print('congratulations... :) you won the match')
+    else:
+            print('so sorry... :( you loose the match')
+z()
+print(f'computer choose...... {computer}')
+# ROCK,PAPER,SCISSOR game
